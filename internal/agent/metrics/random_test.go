ec849595637fd5ab4d2dbcd9360967cc3666294b
Anton Storozhev <antonstorozhev@yandex.ru>
2022-12-23 14:24:10 +0300

    agent: metric collertors tests and benchmarks

---
+++ internal/agent/metrics/random_test.go
@@ -0,0 +1,42 @@
+package metrics
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/tony-spark/metrico/internal/model"
+	"testing"
+)
+
+func TestRandomMetricCollector(t *testing.T) {
+	mc := NewRandomMetricCollector()
+
+	mc.Update()
+	t.Run("has metrics", func(t *testing.T) {
+		assert.NotEmpty(t, mc.Metrics())
+	})
+	t.Run("metrics not empty", func(t *testing.T) {
+		for _, m := range mc.Metrics() {
+			assert.NotEmpty(t, m.ID())
+			assert.NotEmpty(t, m.String())
+		}
+	})
+	t.Run("metrics type", func(t *testing.T) {
+		for _, m := range mc.Metrics() {
+			switch m.Type() {
+			case model.COUNTER:
+				_, ok := m.Val().(int64)
+				assert.True(t, ok)
+			case model.GAUGE:
+				_, ok := m.Val().(float64)
+				assert.True(t, ok)
+			}
+		}
+	})
+}
+
+func BenchmarkRandomMetricCollector(b *testing.B) {
+	mc := NewRandomMetricCollector()
+	for i := 0; i < b.N; i++ {
+		mc.Update()
+		mc.Metrics()
+	}
+}
