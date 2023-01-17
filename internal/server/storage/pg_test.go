0161bf840cb189145f8c8cf5323ba77c6baa19ff
Anton Storozhev <antonstorozhev@yandex.ru>
2023-01-17 23:17:14 +0300

    server: use timeout in pg tests

--- internal/server/storage/pg_test.go
+++ internal/server/storage/pg_test.go
@@ -32,80 +32,96 @@ func (suite *PgTestSuite) SetupSuite() {
 func (suite *PgTestSuite) TestPgStorage() {
 	r := suite.pgm.mdb
 	suite.Run("gauge not found", func() {
-		gauge, err := r.GetGaugeByName(context.Background(), "test")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		gauge, err := r.GetGaugeByName(ctx, "test")
 		assert.Nil(suite.T(), gauge)
 		assert.Nil(suite.T(), err)
 	})
 	suite.Run("gauge saved and found", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		name := "test1"
 		suite.gs = append(suite.gs, name)
-		gauge, err := r.SaveGauge(context.Background(), name, float64(3.14))
+		gauge, err := r.SaveGauge(ctx, name, float64(3.14))
 		assert.NotNil(suite.T(), gauge)
 		assert.Nil(suite.T(), err)
-		gauge, err = r.GetGaugeByName(context.Background(), name)
+		gauge, err = r.GetGaugeByName(ctx, name)
 		assert.NotNil(suite.T(), gauge)
 		assert.Nil(suite.T(), err)
 	})
 	suite.Run("gauge value", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		name := "test2"
 		suite.gs = append(suite.gs, name)
 		value := float64(3.15)
-		gauge1, err := r.SaveGauge(context.Background(), name, value)
+		gauge1, err := r.SaveGauge(ctx, name, value)
 		assert.NotNil(suite.T(), gauge1)
 		assert.NoError(suite.T(), err)
-		gauge2, err := r.GetGaugeByName(context.Background(), name)
+		gauge2, err := r.GetGaugeByName(ctx, name)
 		assert.NotNil(suite.T(), gauge2)
 		assert.NoError(suite.T(), err)
 		assert.Equal(suite.T(), gauge2.Value, value)
 	})
 	suite.Run("get all gauges", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		for i := 3; i < 6; i++ {
 			name := "test" + fmt.Sprint(i)
 			suite.gs = append(suite.gs, name)
 			value := 2.71
-			_, err := r.SaveGauge(context.Background(), name, value)
+			_, err := r.SaveGauge(ctx, name, value)
 			assert.NoError(suite.T(), err)
 		}
-		gs, err := r.getAllGauges(context.Background())
+		gs, err := r.getAllGauges(ctx)
 		assert.NoError(suite.T(), err)
 		assert.True(suite.T(), len(gs) >= 3)
 	})
 	suite.Run("counter not found", func() {
-		counter, err := r.GetCounterByName(context.Background(), "test")
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		counter, err := r.GetCounterByName(ctx, "test")
 		assert.Nil(suite.T(), counter)
 		assert.Nil(suite.T(), err)
 	})
 	suite.Run("counter saved and found", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		name := "test1"
 		suite.cs = append(suite.cs, name)
-		counter, err := r.AddAndSaveCounter(context.Background(), name, int64(314))
+		counter, err := r.AddAndSaveCounter(ctx, name, int64(314))
 		assert.NotNil(suite.T(), counter)
 		assert.Nil(suite.T(), err)
-		counter, err = r.GetCounterByName(context.Background(), name)
+		counter, err = r.GetCounterByName(ctx, name)
 		assert.NotNil(suite.T(), counter)
 		assert.Nil(suite.T(), err)
 	})
 	suite.Run("counter value", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		name := "test2"
 		suite.cs = append(suite.cs, name)
 		values := []int64{1, 4, 5}
 		sums := []int64{1, 5, 10}
 		for i := 0; i < len(values); i++ {
-			counter, err := r.AddAndSaveCounter(context.Background(), name, values[i])
+			counter, err := r.AddAndSaveCounter(ctx, name, values[i])
 			assert.NotNil(suite.T(), counter)
 			assert.Nil(suite.T(), err)
 			assert.Equal(suite.T(), counter.Value, sums[i])
 		}
 	})
 	suite.Run("get all counters", func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 		for i := 3; i < 6; i++ {
 			name := "test" + fmt.Sprint(i)
 			suite.cs = append(suite.cs, name)
 			value := int64(33)
-			_, err := r.SaveCounter(context.Background(), name, value)
+			_, err := r.SaveCounter(ctx, name, value)
 			assert.NoError(suite.T(), err)
 		}
-		gs, err := r.getAllCounters(context.Background())
+		gs, err := r.getAllCounters(ctx)
 		assert.NoError(suite.T(), err)
 		assert.True(suite.T(), len(gs) >= 3)
 	})
